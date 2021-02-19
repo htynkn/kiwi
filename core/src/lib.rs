30253beba8859c24a94c15b76918a1ebe6360704
Huang Yunkun <htynkn@gmail.com>
2021-02-19 21:38:21 +0800

    add rust

---
+++ core/src/lib.rs
@@ -0,0 +1,14 @@
+use std::ffi::{CStr, CString};
+use std::os::raw::c_char;
+
+#[no_mangle]
+pub extern "C" fn rust_greeting(to: *const c_char) -> *mut c_char {
+    let c_str = unsafe { CStr::from_ptr(to) };
+    let recipient = match c_str.to_str() {
+        Err(_) => "there",
+        Ok(string) => string,
+    };
+    CString::new("Hello ".to_owned() + recipient)
+        .unwrap()
+        .into_raw()
+}
